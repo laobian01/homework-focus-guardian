e8d5c5cea8ea9924317bb95067450c955f9f89ae
laobian01 <161675348+laobian01@users.noreply.github.com>
2025-11-23 12:30:16 +0800

    Add files via upload

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,8 @@
-// /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly API_KEY: string;
+    [key: string]: string | undefined;
+  }
+}
